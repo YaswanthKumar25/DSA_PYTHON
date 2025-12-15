d8dbf50749b3599a0ee3597715743055efdcf170
Yaswanth <dasariyeshwanthkumar@gmail.com>
2025-12-15 20:16:10 +0530

    basic_recurssion

---
+++ day4.py
@@ -0,0 +1,45 @@
+#print name N time
+def fun(n):
+    if n > 5:
+        return
+    print("yaswanth")
+    n+=1
+    fun(n)
+n=1
+fun(n)
+
+
+
+#print 1 to n
+def fun(n):
+    if n >5:
+        return
+    print(n)
+    n+=1
+    fun(n)
+n=1
+fun(n)
+
+
+
+#print n to 1
+def fun(n):
+    if n<0:
+        return
+    print(n)
+    n-=1
+    fun(n)
+n=5
+fun(n)
+
+
+
+#print 1 to n by backtrack
+def fun(n):
+    if n < 0:
+        return
+    fun(n-1)
+    print(n)
+
+n=5
+fun(n)
