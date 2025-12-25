9a18a2557d61801d9ff494bca9569da0b76b6b32
Yaswanth <dasariyeshwanthkumar@gmail.com>
2025-12-25 20:48:26 +0530

    day12

--- day12-Arrays.py
+++ day12-Arrays.py
@@ -1,9 +1,16 @@
 #Largest Element in Array
 #Brute
+# The code snippet `arr=[3,5,4,2,1] arr.sort() print(arr[-1])` is finding the largest element in the
+# array `arr` using a brute force approach.
 arr=[3,5,4,2,1]
 arr.sort()
 print(arr[-1])
 
+# The code snippet is finding the largest element in the array `arr` using an optimal approach. It
+# initializes a variable `largest` with the first element of the array. Then, it iterates through the
+# array and compares each element with the current largest element. If the current element is greater
+# than the current largest element, it updates the `largest` variable to the current element. Finally,
+# it prints the largest element found in the array.
 #Optimal
 arr=[3,5,4,2,1]
 largest=arr[0]
@@ -12,6 +19,8 @@ for i in range(len(arr)):
         largest=arr[i]
 print(largest)
 
+# The code snippet is finding the second largest element in the array `arr` using a brute force
+# approach. Here's a breakdown of the code:
 #Second Largest
 #Brute
 arr=[3,5,4,2,1]
@@ -25,6 +34,8 @@ for i in range(n-1,-1,-1):
         break
 print(Slargest)
 
+# The code snippet you provided is finding the second largest element in the array `arr` using a
+# better approach compared to the brute force method. Here's a breakdown of the code:
 #Better
 arr=[3,5,5,4,2,1]
 largest=arr[0]
@@ -37,6 +48,8 @@ for i in range(len(arr)):
         Slargest=arr[i]
 print(Slargest)
 
+# The code snippet you provided is finding the second largest element in the array `arr` using an
+# optimal approach. Here's a breakdown of the code:
 #optimal
 arr=[3,5,4,2,1,5,6]
 largest=arr[0]
@@ -47,7 +60,11 @@ for i in range(len(arr)):
         largest=arr[i]
 print(Slargest)
 
-#Check Array is Sorted or Not
+    # The function checks whether an array is sorted in ascending order or not.
+    # :param arr: The `arr` parameter is a list of numbers that you want to check if it is sorted or not.
+    # In the provided code snippet, the function `fun` takes an array `arr` as input and checks if the
+    # elements in the array are sorted in non-decreasing order. If the array
+    # :return: "It is a Sorted Array"
 def fun(arr):
     n=len(arr)
     for i in range(1,n):
